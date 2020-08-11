49e4b004f8c6d56a1d6281802c08e215d7f37f30
Kelson Hartle <kelson99hartle@icloud.com>
2020-08-11 10:58:00 -0600

    Created Swift Based Xcode Project.

---
+++ UKnowUtah/UKnowUtah/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UKnowUtah
+//
+//  Created by Kelson Hartle on 8/11/20.
+//  Copyright © 2020 Kelson Hartle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
